ea253e7fa16fa12e870402bf906764b65cb8707f
Toeygenx <thanalast99@gmail.com>
2026-06-12 07:00:10 +0700

    chore: update startup script to apply correct storage and cache directory permissions

--- start.sh
+++ start.sh
@@ -7,5 +7,9 @@ php artisan cache:clear
 echo "Running Database Migrations..."
 php artisan migrate --force
 
+echo "Fixing permissions..."
+chown -R www-data:www-data /var/www/html/storage /var/www/html/bootstrap/cache
+chmod -R 775 /var/www/html/storage /var/www/html/bootstrap/cache
+
 echo "Starting Apache server..."
 apache2-foreground
